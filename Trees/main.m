783f2f390370c88b9b4aefc6dca546b842610bd3
Bharath G M <gmbharath12@gmail.com>
2014-08-03 16:39:53 -0500

    Push to Repository

--- Trees/main.m
+++ Trees/main.m
@@ -7,14 +7,77 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <malloc/malloc.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+typedef struct node* NODE;
+
+NODE getNewNode(int new_data)
+{
+    NODE temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = new_data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+void getVerticalOrder(NODE root, NSMutableDictionary* hashTable,int horizontalDistance)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    NSMutableArray* larray;
+    
+    if (![hashTable objectForKey:[NSString stringWithFormat:@"%d",horizontalDistance]])
+    {
+        larray = [NSMutableArray array];
+        [larray addObject:[NSString stringWithFormat:@"%d",root->data]];
+        [hashTable setObject:larray forKey:[NSString stringWithFormat:@"%d",horizontalDistance]];
+    }
+    else
+    {
+        [[hashTable objectForKey:[NSString stringWithFormat:@"%d",horizontalDistance]] addObject:[NSString stringWithFormat:@"%d",root->data]];
+    }
+         
+    getVerticalOrder(root->left, hashTable, horizontalDistance-1);
+    getVerticalOrder(root->right, hashTable, horizontalDistance+1);
+}
+
+void printVerticalOrder(NODE root)
+{
+    if (root == NULL)
+    {
+        return ;
+    }
+    NSMutableDictionary *hashTable = [[NSMutableDictionary alloc] init];
+    int horizontalDistance = 0;
+    getVerticalOrder(root, hashTable,horizontalDistance);
+    NSLog(@" \n Vertical Order of a tree = %@",hashTable);
+}
+
 
 int main(int argc, const char * argv[])
 {
 
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
+    @autoreleasepool
+    {
+        NODE root = getNewNode(1);
+        root->left = getNewNode(2);
+        root->right = getNewNode(3);
+        root->left->left = getNewNode(4);
+        root->left->right = getNewNode(5);
+        root->right->left = getNewNode(6);
+        root->right->right = getNewNode(7);
+        root->right->left->right = getNewNode(8);
+        root->right->right->right = getNewNode(9);
+        printf("Vertical order traversal is \n");
+        printVerticalOrder(root);
         
     }
     return 0;
